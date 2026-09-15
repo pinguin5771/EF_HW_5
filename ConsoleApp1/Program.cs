d93563c3b1a56c41cad8e8ec5d50ea71d454d83e
IrynaPolovko <pinguin5771@gmail.com>
2026-09-15 23:18:17 +0300

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace ConsoleApp1
+{
+    public class Device
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty; // TV, Lights, PC,
+        //Refrigerator,...
+}
+    public class EnergySource
+    {
+        public int Id { get; set; }
+        // property does not exists -> // Battery, Grid, Solar
+    }
+    public class ConsuptionLog
+    {
+        public int Id { get; set; }
+        public DateTime DateTime { get; set; }
+        public double UsedEnergy { get; set; }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
